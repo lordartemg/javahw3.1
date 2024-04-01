2d026c027b4fa6fd71273cd5438db39927d95be3
Grdilyan Artem <grd.artems@gmail.com>
2024-04-01 17:37:34 +0300

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+    public int calculate(int a) {
+        int result;
+        result = a / 20;
+        return result;
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 15_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
+    }
+}
+
+
